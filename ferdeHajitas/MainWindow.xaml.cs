99aa1244e5bcea1011a1e126368cb5f9bb659cf9
zimavagyok <csizmadiagergoo@gmail.com>
2020-10-17 21:59:10 +0200

    Unit optimization, extra features

--- ferdeHajitas/MainWindow.xaml.cs
+++ ferdeHajitas/MainWindow.xaml.cs
@@ -21,8 +21,9 @@ namespace ferdeHajitas
     /// </summary>
     public partial class MainWindow : Window
     {
-        static double m=-1, v0=-1, alfa=1, cd=-1, g = 9.81;
+        static double m=-1, v0=-1, alfa=1, cd=-1, g = 0;
         Canvas canvas;
+        bool loaded = false;
 
         public MainWindow()
         {
@@ -71,7 +72,23 @@ namespace ferdeHajitas
 
         private void playButton_Click(object sender, RoutedEventArgs e)
         {
-            canvas.Unit = 1;
+            if(v0 < 2.5)
+            {
+                canvas.Unit =150;
+            }
+            else if(v0<10)
+            {
+                canvas.Unit = 80;
+            }
+            else if(v0<40)
+            {
+                canvas.Unit = 5;
+            }
+            else
+            {
+                canvas.Unit = 1;
+            }
+            
             canvas.Clear(true, false);
             List<double> x = new List<double>();
             List<double> y = new List<double>();
@@ -82,8 +99,8 @@ namespace ferdeHajitas
             double t = 0;
             double drag = cd * v0 * v0;
             double ax = -(drag * Math.Cos(alfa / 180 * Math.PI)) / m;
-            double ay = -g-(drag * Math.Sin(alfa / 180 * Math.PI)) / m;
-            double step = .2; // Ez a pontosság és ez van legjobban hatással a teljesítményre. Minél kisebb annál pontosabb.
+            double ay = -g-(drag * Math.Sin(alfa / 180 * Math.PI) / m);
+            double step = .05; // Ez a pontosság és ez van legjobban hatással a teljesítményre. Minél kisebb annál pontosabb.
             int counter = 0;
             while(y[counter]>=0)
             {
@@ -92,34 +109,55 @@ namespace ferdeHajitas
                 vx += step * ax;
                 vy += step * ay;
 
-                x.Add(step * vx);
-                y.Add(step * vy);
+                x.Add(x[counter]+step * vx);
+                y.Add(y[counter]+step * vy);
 
                 canvas.DrawLine(x[counter], y[counter], x[counter+1], y[counter+1]);
 
                 double velocity = Math.Sqrt(vx * vx + vy * vy);
                 drag = cd * velocity * velocity;
                 ax = -(drag * Math.Cos(alfa / 180 * Math.PI)) / m;
-                ay = -g - (drag * Math.Sin(alfa / 180 * Math.PI)) / m;
+                ay = -g - (drag * Math.Sin(alfa / 180 * Math.PI) / m);
 
                 counter++;
             }
+            tav.Content = x[counter];
+            hajT.Content = t;
+            hajMax.Content = y.Max(x => x);
+        }
+
+        private void gTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double temp;
+            if (gTB.Text != String.Empty && double.TryParse(gTB.Text, out temp) && temp >= 0)
+            {
+                g = double.Parse(gTB.Text);
+            }
+            else if (gTB.Text != String.Empty)
+            {
+                MessageBox.Show("Hibás adatbevitel!");
+            }
+            checkIfNoEmpty();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             canvas = new Canvas(canvasControl);
+            loaded = true;
         }
 
         private void SliderGetValue(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            alfa = alfaSlider.Value;
-            //fok.Content = alfa.ToString();
+            if(loaded)
+            {
+                alfa = alfaSlider.Value;
+                fok.Content = alfa.ToString();
+            }
         }
 
         private void checkIfNoEmpty()
         {
-            if (m > 0 && v0 > 0 && cd > 0)
+            if (m > 0 && v0 > 0 && cd >= 0 && g > 0)
             {
                 playButton.IsEnabled = true;
             }
